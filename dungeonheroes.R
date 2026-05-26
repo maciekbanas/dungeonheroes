2cfb7c3f2e4b04261ea62da15f4838ae0c701052
Maciej Banaś <74212933+maciekbanas@users.noreply.github.com>
2026-05-26 21:20:16 +0200

    Reduce wizard alert lag by throttling talk animation

--- dungeonheroes.R
+++ dungeonheroes.R
@@ -13,6 +13,7 @@ server <- function(input, output, session) {
   shiny::addResourcePath("assets", "assets")
   
   life_points <- 100
+  wizard_is_talking <- FALSE
 
   game$set_shiny_session()
 
@@ -146,7 +147,10 @@ server <- function(input, output, session) {
     object_two = "wizard",
     callback_fun = function(evt) {
       talk_btn$show()
-      wizard$play_animation("wizard_talk", 2e3)
+      if (!wizard_is_talking) {
+        wizard_is_talking <<- TRUE
+        wizard$play_animation("wizard_talk", 2e3)
+      }
     },
     input = input
   )
@@ -155,6 +159,7 @@ server <- function(input, output, session) {
     object_two_name = "wizard",
     callback_fun = function(evt) {
       talk_btn$hide()
+      wizard_is_talking <<- FALSE
       wizard$play_animation("wizard_idle")
     },
     input = input
